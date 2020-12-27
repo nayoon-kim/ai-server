90bbc8dbe33e4d8aa3890fe3c91c65fb1f8a8648
김나윤 <belloyv@gmail.com>
2020-12-27 16:11:47 +0900

    default information

--- app.py
+++ app.py
@@ -4,9 +4,19 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    return render_template('index.html',title='AI-SERVER')
 
+@app.route('/<pagename>')
+def hello(pagename):
+    return pagename
 
+@app.route('/animal')
+def animal():
+    return render_template('animal.html')
+
+@app.route('/dog')
+def dog():
+    return render_template('dog.html')
 if __name__ == '__main__':
     app.run(debug=True)
